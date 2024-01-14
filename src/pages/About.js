f42c424573847dca72b85d72a84e31b04ca73243
Leo-Iz <15.alireza@gmail.com>
2024-01-14 10:22:47 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum harum sit
+        accusamus doloremque ducimus non quisquam quas ab reiciendis laboriosam,
+        sapiente et ex velit ipsum recusandae vel dolore repudiandae. Commodi
+        nobis rem sunt dolores tempore animi, aperiam ex. Nesciunt, quam!
+      </p>
+    </section>
+  )
+}
+
+export default About
